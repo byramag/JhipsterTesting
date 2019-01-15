e701272071d810ec03277eef113dd8aff5fa4c13
Matthew Pointer <pointermc@mymail.vcu.edu>
2019-01-15 10:03:28 -0500

    Added Embedded Google Form Application - format soon

--- TaManagementApp/src/databaseconnection.js
+++ TaManagementApp/src/databaseconnection.js
@@ -4,17 +4,38 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 
-const port = 4242 || process.env.port;
+const port = 4200 || process.env.port;
 const app = express();
 
 var con = mysql.createConnection({
-  host: "",
+  host: "10.142.0.2",
   user: "cs313tams",
   password: "vcu",
   database: 'cs313tams:us-east1:myinstance'
 });
 
-con.connect(function(err) {
-  if (err){ throw err;}
+con.connect(function (err) {
+  if (err) { throw err; }
   console.log("Connected!");
+});
+
+const initDB = function () {
+  return new Promise((resolve, reject) => {
+    db.connect(err => {
+      if (err) {
+        console.error('Unable to connect to database.', err);
+        console.error('Retrying in 5s.');
+        setTimeout(() => initDB().then(resolve), 5000);
+      } else {
+        console.log('Connected to mysql!');
+        resolve();
+      }
+    });
+  });
+};
+
+console.log('Waiting for database connection...');
+initDB().then(() => {
+  app.listen(port);
+  console.log(`Running on port ${port}.`);
 });
