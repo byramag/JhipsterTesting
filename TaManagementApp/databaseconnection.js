c6cd91f0945b50b067c1c5cd4b902a1810a04628
mTran1997 <32003710+mTran1997@users.noreply.github.com>
2019-01-20 20:13:02 -0500

    databaseconnection.js made connection

--- TaManagementApp/databaseconnection.js
+++ TaManagementApp/databaseconnection.js
@@ -1,79 +1,66 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
-const session = require('express-session');
+//const bcrypt = require('bcrypt');
+//const session = require('express-session');
 
 const port = 8080 || process.env.port;
 const app = express();
-
-app.use('/', express.static('TAMANAGEMENTAPP'));
+//app.use('/', express.static('TAMANAGEMENTAPP'));
 
 var con = mysql.createConnection({
-  host: "10.142.0.2",
-  user: "cs313tams",
+  host: "104.196.121.215",
+  user: "tester",
   password: "vcu",
-  database: 'cs313tams:us-east1:myinstance'
-});
-
-const initDB = function() {
-  return new Promise((resolve, reject) => {
-    con.connect(err => {
-      if (err) {
-        console.error('Unable to connect to database.', err);
-        console.error('Retrying in 5s.');
-        setTimeout(() => initDB().then(resolve), 5000);
-      } else {
-        console.log('Connected.');
-        resolve();
-      }
-    });
-  });
-};
-
-const query = function(str, ...params) {
-  return new Promise((resolve, reject) => {
-    con.query(str, params, (error, results, fields) => {
-      if (error) {
-        reject(error);
-        console.error({
-          invocation: [str, ...params],
-          error
-        });
-      } else {
-        resolve(results);
-      }
-    });
-  });
-};
-
-con.connect(function(err) {
-  if (err){ throw err;}
-  console.log("Connected!");
+  database: 'tamanagement',
+  //port: 8080
 });
 
 console.log('Waiting for database...');
-con.connect(function (err) {
-  if (err) { throw err; }
-  console.log("Connected!");
-});
-
-const initDB = function () {
-  return new Promise((resolve, reject) => {
-    db.connect(err => {
-      if (err) {
-        console.error('Unable to connect to database.', err);
-        console.error('Retrying in 5s.');
-        setTimeout(() => initDB().then(resolve), 5000);
-      } else {
-        console.log('Connected to mysql!');
-        resolve();
-      }
+con.connect(function(err) {
+  if (err){
+    console.log("error connecting", err);
+    throw err;
+  }else{
+      con.query("SELECT * FROM TA", function (err, result, fields) {
+      console.log(result);
     });
-  });
-};
-
-initDB().then(() => {
-  app.listen(port);
-  console.log(`Running on port ${port}.`);
+  console.log("Connected!");
+  }
 });
+
+// const initDB = function() {
+//   return new Promise((resolve, reject) => {
+//     con.connect(err => {
+//       if (err) {
+//         console.error('Unable to connect to database.', err);
+//         console.error('Retrying in 5s.');
+//         setTimeout(() => initDB().then(resolve), 5000);
+//       } else {
+//         console.log('Connected.');
+//         resolve();
+//       }
+//     });
+//   });
+// };
+
+// const query = function(str, ...params) {
+//   return new Promise((resolve, reject) => {
+//     con.query(str, params, (error, results, fields) => {
+//       if (error) {
+//         reject(error);
+//         console.error({
+//           invocation: [str, ...params],
+//           error
+//         });
+//       } else {
+//         resolve(results);
+//       }
+//     });
+//   });
+// };
+
+// initDB().then(() => {
+//   app.listen(port);
+//   console.log(`Running on port ${port}.`);
+// });
