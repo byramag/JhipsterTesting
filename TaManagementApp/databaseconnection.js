a0ef1a03d65da0cd50196bb040a96c9cc02036f2
Matthew Pointer <pointermc@mymail.vcu.edu>
2019-01-23 20:50:58 -0500

    Update

--- TaManagementApp/databaseconnection.js
+++ TaManagementApp/databaseconnection.js
@@ -1,46 +1,67 @@
 //node databaseconnection.js -> command line
 //query by...
 // con.query("SELECT * FROM course", function (err, result, fields) {
-      //console.log(result); (you can do whenever you want with result in here)
-    //});
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
+//console.log(result); (you can do whenever you want with result in here)
+//});
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
 //const bcrypt = require('bcrypt');
 //const session = require('express-session');
 
-const port = 8080 || process.env.port;
-const app = express();
+// const port = 8080 || process.env.port;
+// const app = express();
 //app.use('/', express.static('TAMANAGEMENTAPP'));
 
-var con = mysql.createConnection({
-  host: "104.196.121.215",
-  user: "tester",
-  password: "vcu",
-  database: 'tamanagement',
-});
 
-console.log('Waiting for database...');
-con.connect(function(err) {
-  if (err){
-    console.log("error connecting", err);
-    throw err;
-  }else{
-     // con.query("SELECT * FROM course", function (err, result, fields) {
-      //console.log(result);
-    //});
-  console.log("Connected!");
-  }
-});
+const mysql = require('mysql');
 
-const query = function(str, ...params){
-  return new Promise((resolve, reject) => {
-    con.query(str, param, (err, result, fields) => {
-      if(err){
-        reject(err);
-      }else{
-        resolve(result);
-      }
-    });
+// var con = mysql.createConnection({
+//   host: "104.196.121.215",
+//   user: "tester",
+//   password: "vcu",
+//   database: 'tamanagement',
+// });
+
+const test = function (str, ...params) {
+  console.log(str);
+}
+
+const query = function (str, ...params) {
+
+  console.log('!!!');
+
+  var con = mysql.createConnection({
+    host: "104.196.121.215",
+    user: "tester",
+    password: "vcu",
+    database: 'tamanagement',
   });
+
+  console.log('Waiting for database...');
+  con.connect(function (err) {
+    if (err) {
+      console.log("error connecting", err);
+      throw err;
+    } else {
+      con.query(str, function (err, result, fields) {
+        console.log(result);
+      });
+      console.log("Connected!");
+    }
+  });
+
+
+  // return new Promise((resolve, reject) => {
+  //   con.query(str, param, (err, result, fields) => {
+  //     if(err){
+  //       reject(err);
+  //     }else{
+  //       resolve(result);
+  //     }
+  //   });
+  // });
 };
+
+query("SELECT * FROM TA");
+
