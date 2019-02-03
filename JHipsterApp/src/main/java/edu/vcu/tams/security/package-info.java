4753f20619b5ee7e29f475af197d147a485426f5
Abigail Byram <byramabigail@gmail.com>
2019-02-03 11:32:17 -0500

    Initial application generated by JHipster-5.8.0

---
+++ JHipsterApp/src/main/java/edu/vcu/tams/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package edu.vcu.tams.security;
---
+++ JHipsterApp/src/main/java/edu/vcu/tams/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package edu.vcu.tams.web.rest.vm;
