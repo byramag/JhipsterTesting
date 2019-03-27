13f67c4294652ceb127f6a1e47736a802cbb5485
Abigail Byram <byramabigail@gmail.com>
2019-03-27 17:43:26 -0400

    Filling TA handbook with content draft

--- JhipsterAppUpdate/src/main/webapp/app/taHandbook/handbook/handbook.component.ts
+++ JhipsterAppUpdate/src/main/webapp/app/taHandbook/handbook/handbook.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { IApplicant } from 'app/shared/model/applicant.model';
-import { ApplicantService } from 'app/entities/applicant/applicant.service';
 
 @Component({
     selector: 'jhi-handbook',
